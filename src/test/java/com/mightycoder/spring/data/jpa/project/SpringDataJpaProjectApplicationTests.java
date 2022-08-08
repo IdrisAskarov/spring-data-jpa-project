548bf43aa980104e242af45c1baa92c1bb420bd0
Idris Askarov <askid.me@gmail.com>
2022-08-07 20:18:22 -0500

    newly created empty project

---
+++ src/test/java/com/mightycoder/spring/data/jpa/project/SpringDataJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mightycoder.spring.data.jpa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
