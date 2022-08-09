03462dccc4ef95fa6d32be0a9fab4bcddcd3b434
Idris Askarov <askid.me@gmail.com>
2022-08-09 16:40:53 -0500

    Implemented Paging and Sorting

--- src/main/java/com/mightycoder/spring/data/jpa/project/repository/CourseRepository.java
+++ src/main/java/com/mightycoder/spring/data/jpa/project/repository/CourseRepository.java
@@ -1,7 +1,14 @@
 package com.mightycoder.spring.data.jpa.project.repository;
 
 import com.mightycoder.spring.data.jpa.project.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Page<Course> findByCourseTitleContaining(String title, Pageable pageable);
 }
--- src/test/java/com/mightycoder/spring/data/jpa/project/repository/CourseRepositoryTest.java
+++ src/test/java/com/mightycoder/spring/data/jpa/project/repository/CourseRepositoryTest.java
@@ -6,6 +6,10 @@ import com.mightycoder.spring.data.jpa.project.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,13 +20,13 @@ class CourseRepositoryTest {
     private CourseRepository courseRepository;
 
     @Test
-    public void printCourses(){
+    public void printCourses() {
         var courses = courseRepository.findAll();
         courses.forEach(System.out::println);
     }
 
     @Test
-    public void saveCourse(){
+    public void saveCourse() {
         Course course = Course.builder()
                 .credit(1)
                 .courseTitle("ALG")
@@ -32,7 +36,7 @@ class CourseRepositoryTest {
     }
 
     @Test
-    public void saveCourseWithTeacherObject(){
+    public void saveCourseWithTeacherObject() {
         Faker faker = new Faker();
         Course course = Course
                 .builder()
@@ -48,4 +52,59 @@ class CourseRepositoryTest {
         courseRepository.save(course);
         assertNotNull(course.getCourseId());
     }
+
+    @Test
+    public void findAllPagination() {
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+        Pageable secondPageWithThooRecords = PageRequest.of(1, 2);
+
+        var coursePages = courseRepository.findAll(firstPageWithThreeRecords);
+        var courses = coursePages.getContent();
+        var totalElements = coursePages.getTotalElements();
+        var totalPages = coursePages.getTotalPages();
+        courses.forEach(course -> System.out.println("courseId: " + course.getCourseId()));
+        System.out.println("totalElements: " + totalElements);
+        System.out.println("totalPages: " + totalPages);
+
+
+        coursePages = courseRepository.findAll(secondPageWithThooRecords);
+        courses = coursePages.getContent();
+        totalElements = coursePages.getTotalElements();
+        totalPages = coursePages.getTotalPages();
+        courses.forEach(course -> System.out.println("courseId: " + course.getCourseId()));
+        System.out.println("totalElements: " + totalElements);
+        System.out.println("totalPages: " + totalPages);
+
+    }
+
+    @Test
+    public void findAllSorting() {
+        var sortByTitle = PageRequest.of(
+                0,
+                5,
+                Sort.by("courseTitle"));
+
+        var sortByCreditDesc = Sort.by("credit").descending();
+        var sortByCreditDescPages = PageRequest.of(
+                0,
+                8,
+                sortByCreditDesc);
+        var sortByTitleAndCreditDesc = PageRequest.of(
+                0,
+                20,
+                Sort.by("courseTitle").descending().and(Sort.by("credit").descending())
+        );
+
+        var pages = courseRepository.findAll(sortByTitleAndCreditDesc);
+        var courses = pages.getContent();
+        courses.forEach(System.out::println);
+    }
+
+    @Test
+    public void findByCourseTitleContaining() {
+        var firstPage = PageRequest.of(0, 10,Sort.by("courseId").descending());
+        var courses = courseRepository.findByCourseTitleContaining("D", firstPage).getContent();
+        courses.forEach(System.out::println);
+    }
+
 }
